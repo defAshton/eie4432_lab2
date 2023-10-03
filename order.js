af8d7a41c9ab55f17285a6001cc27de4912db441
defAshton <137738630+defAshton@users.noreply.github.com>
2023-10-03 23:46:50 +0800

    Add files via upload

--- order.js
+++ order.js
@@ -1,93 +1,89 @@
-function culculatePrice() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+function calculatePrice() {
+    const radioButtons = document.querySelectorAll('input[type="radio"]');
     const selectElement = document.getElementById("drink");
     const selectedDrink = selectElement.value;
-    let Price = 0;
-    checkboxes.forEach(function (checkbox) {
-        checkbox.addEventListener('change', function () {
+    let price = 0;
+  
+    radioButtons.forEach(function (radioButton) {
+        radioButton.addEventListener('change', function () {
             updatePrice();
         });
     });
+  
     function updatePrice() {
-        let selectedSize = "";
-        checkboxes.forEach(function (checkbox) {
-            if (checkbox.checked) {
-                selectedSize = checkbox.value;
-            }
-        });
-        if (selectedSize !== "") {
-            if (selectedDrink == "bubble-milktea") {
-                if (selectedSize == "small") {
-                    Price = 20;
-                } else if (selectedSize == "medium") {
-                    Price = 25;
-                } else if (selectedSize == "large") {
-                    Price = 30;
-                }
-            } else if (selectedDrink == "iced-latte") {
-                if (selectedSize == "small") {
-                    Price = 20;
-                } else if (selectedSize == "medium") {
-                    Price = 25;
-                } else if (selectedSize == "large") {
-                    Price = 30;
+        const selectedRadioButton = document.querySelector('input[type="radio"]:checked');
+  
+        if (selectedRadioButton) {
+            const selectedSize = selectedRadioButton.value;
+  
+            if (selectedDrink === "bubble-milktea" || selectedDrink === "iced-latte") {
+                if (selectedSize === "small") {
+                    price = 20;
+                } else if (selectedSize === "medium") {
+                    price = 25;
+                } else if (selectedSize === "large") {
+                    price = 30;
                 }
             }
         } else {
-            Price = 0;
+            price = 0;
         }
+  
         const priceElement = document.getElementById("price");
-        priceElement.textContent = Price.toFixed(2);
+        priceElement.textContent = price.toFixed(2);
     }
-    updatePrice(); 
+  
+    updatePrice();
+  
     if (selectedDrink === "") {
         alert("Please select a drink");
-        checkboxes.forEach(function (checkbox) {
-          checkbox.checked = false;
+        radioButtons.forEach(function (radioButton) {
+            radioButton.checked = false;
         });
-      }
     }
-function validateForm(){
+}
+  
+function validateForm() {
     const name = document.getElementById("name").value;
     const drink = document.getElementById("drink").value;
-    const sizeInputs = document.querySelectorAll('input[name^="size-option"]:checked');
-    const iceInputs = document.querySelectorAll('input[name^="ice-option"]:checked');
-    const sweetnessInputs = document.querySelectorAll('input[name^="sweetness-option"]:checked');
-
-    const sizes = Array.from(sizeInputs).map(input => input.value);
-    const ices = Array.from(iceInputs).map(input => input.value);
-    const sweetnesses = Array.from(sweetnessInputs).map(input => input.value);
-    if (name.trim()==""){
-        alert("Please enter your name.")
+    const sizeInput = document.querySelector('input[name="size-option"]:checked');
+  
+    if (name.trim() == "") {
+        alert("Please enter your name.");
         return false;
     }
-    if (drink=="please select"){
-        alert("Please select a drink first.")
+  
+    if (drink == "please select") {
+        alert("Please select a drink first.");
         return false;
     }
+  
+    if (!sizeInput) {
+        alert("Please select a size.");
+        return false;
+    }
+  
     return true;
 }
-function placeOrder(event){
+  
+function placeOrder(event) {
     event.preventDefault();
-    flag=validateForm();
-    if (flag==true){
+    const isValid = validateForm();
+  
+    if (isValid) {
         const name = document.getElementById("name").value;
         const drink = document.getElementById("drink").value;
-        const sizeInputs = document.querySelectorAll('input[name^="size-option"]:checked');
-        const iceInputs = document.querySelectorAll('input[name^="ice-option"]:checked');
-        const sweetnessInputs = document.querySelectorAll('input[name^="sweetness-option"]:checked');
-
-        const sizes = Array.from(sizeInputs).map(input => input.value);
-        const ices = Array.from(iceInputs).map(input => input.value);
-        const sweetnesses = Array.from(sweetnessInputs).map(input => input.value);
-        const orderData = [name, drink, sizes, ices, sweetnesses];
+        const sizeInput = document.querySelector('input[name="size-option"]:checked');
+        const size = sizeInput.value;
+  
+        const orderData = [name, drink, size];
         const orderDataString = JSON.stringify(orderData);
-        localStorage.setItem("orders", orderDataString);
-        if (drink=="third-drink"){
-            alert("Sorry, third drink is unavailable now!");
-        }else{
-            alert("Order placed successfully! Thank you for your order.");
-        }
-        
+        localStorage.setItem("order", orderDataString);
+  
+    if (drink === "third-drink") {
+        alert("Sorry, the third drink is unavailable now!");
+    } else {
+        alert("Order placed successfully! Thank you for your order.");
+    }
     }
 }
